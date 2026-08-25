99a1c8d01a3d5f537617df98527d8ab7302afd6e
Bahito04 <muhammedbah740@gmail.com>
2026-08-25 12:14:19 +0000

    Added clipboard function

--- Random.js
+++ Random.js
@@ -1,12 +1,17 @@
 const btnEl = document.querySelector(".btn")
 const inputEl = document.getElementById("input")
 const copyIconEl = document.querySelector(".fa-copy")
+const alertContainerEl = document.querySelector(".alert-container")
 
 btnEl.addEventListener("click", ()=>{
     createPassword()
 }) 
 copyIconEl.addEventListener("click", () =>{
     copyPassword()
+    alertContainerEl.classList.remove("active")
+    setTimeout(()=>{
+        alertContainerEl.classList.add("active")
+    } ,2000)
 })
 function createPassword(){
     const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -21,8 +26,9 @@ function createPassword(){
         
     }
     inputEl.value = password 
-
+ alertContainerEl.innerText = password + " copied!"
 }
+
 function copyPassword(){
     inputEl.select()
     inputEl.setSelectionRange(0,9999)
